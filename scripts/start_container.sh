82e5c27c3b137aa1f60cc49746774b4907f3d1f7
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-01-19 22:24:04 +0000

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sholly333/hotel-app:latest
 
 # Run the Docker image as a container
-docker run -dit -p 8083:80 sholly333/hotel-app
+docker run -dit -p 8084:80 sholly333/hotel-app
